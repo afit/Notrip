b3ba867f60464aec15f216a263b0192d700c29a1
Aidan Fitzpatrick <aidan@reincubate.com>
2006-02-02 17:41:24 +0000

    Removed capo property Started frets at 0 rather than 1 Possible offset bug remains?

--- Notrip/StringedInstrument.cs
+++ Notrip/StringedInstrument.cs
@@ -22,8 +22,7 @@ namespace LothianProductions.Notrip {
 				new Tuning( Note.B, 4 ),
 				new Tuning( Note.E, 4 )
 			};
-			mFrets = 24;
-			mCapo = 0;
+			mFrets = 23;
 			mHorizontal = true;
 			mShowFrequencies = false;
 			InitializeComponent();
@@ -70,16 +69,6 @@ namespace LothianProductions.Notrip {
 				Invalidate();
 			}
 		}
-
-		// FIXME deprecate this?
-		protected int mCapo;
-		public int Capo {
-			get{ return mCapo; }
-			set{
-				mCapo = value;
-				Invalidate();
-			}
-		}
 	
 		protected bool mHorizontal;
 		public bool Horizontal {
@@ -102,71 +91,71 @@ namespace LothianProductions.Notrip {
 		protected List<Sound> mPreviousSounds = new List<Sound>();
 		public void Highlight( List<Sound> sounds, bool playHighlighted ) {
 			foreach( Sound sound in mPreviousSounds ) {
-				if( playHighlighted )
-					AudioMonitor.Instance().PlayNote( AudioMonitor.StepToFrequency( sound.Step ), 0, this );
+			    if( playHighlighted )
+			        AudioMonitor.Instance().PlayNote( AudioMonitor.StepToFrequency( sound.Step ), 0, this );
 					
 			    for( int instrumentString = 0; instrumentString < Strings.Length; instrumentString++ ) {		
-					int step = AudioMonitor.TuningToStep( Strings[instrumentString] );
+			        int step = AudioMonitor.TuningToStep( Strings[instrumentString] );
 					
-					if( step <= sound.Step && step + Frets > sound.Step ) {
-						int fret = sound.Step - step + 1;
+			        if( step <= sound.Step && step + Frets > sound.Step ) {
+			            int fret = sound.Step - step + 1;
 						
-						if( mLeftHanded )
-							fret = Frets + 1 - fret;
+			            if( mLeftHanded )
+			                fret = mFrets - fret;
 							
-						DrawFingering( Graphics.FromHwnd( Handle ), instrumentString + 1, fret, Brushes.White );
-					}
+			            DrawFingering( Graphics.FromHwnd( Handle ), instrumentString + 1, fret, Brushes.White );
+			        }
 			    }
 			}
 			    
 			foreach( Sound sound in sounds ) {
-				if( playHighlighted )
-					AudioMonitor.Instance().PlayNote( AudioMonitor.StepToFrequency( sound.Step ), 0, this );
+			    if( playHighlighted )
+			        AudioMonitor.Instance().PlayNote( AudioMonitor.StepToFrequency( sound.Step ), 0, this );
 					
 			    for( int instrumentString = 0; instrumentString < Strings.Length; instrumentString++ ) {		
-					int step = AudioMonitor.TuningToStep( Strings[instrumentString] );
+			        int step = AudioMonitor.TuningToStep( Strings[instrumentString] );
 
-					if( step <= sound.Step && step + Frets > sound.Step ) {
-						int fret = sound.Step - step + 1;
+			        if( step <= sound.Step && step + Frets > sound.Step ) {
+			            int fret = sound.Step - step + 1;
 						
-						if( mLeftHanded )
-							fret = Frets + 1 - fret;
+			            if( mLeftHanded )
+			                fret = mFrets - fret;
 							
-						DrawFingering(
-							Graphics.FromHwnd( Handle ), instrumentString + 1, fret,
-							new SolidBrush( Color.FromArgb( 255, 255 - (sound.Amplitude >= 64 ? 255 : sound.Amplitude * 4), 255 - (sound.Amplitude >= 64 ? 255 : sound.Amplitude * 4) ) )
-						);
-					}
+			            DrawFingering(
+			                Graphics.FromHwnd( Handle ), instrumentString + 1, fret,
+			                new SolidBrush( Color.FromArgb( 255, 255 - (sound.Amplitude >= 64 ? 255 : sound.Amplitude * 4), 255 - (sound.Amplitude >= 64 ? 255 : sound.Amplitude * 4) ) )
+			            );
+			        }
 			    }
 			}
 			mPreviousSounds = sounds;
 		}
 	
 		protected void FindFingering( int x, int y, out int fret, out int instrumentString ) {
-			if( Strings.Length == 0 || Frets == 0 ) {
+			if( Strings.Length == 0 ) {
 				fret = 0;
 				instrumentString = 0;
 			} else {
-				fret = (int) Math.Round( (float) x / (float) (Width / (Frets + 1)), 0 );
+				fret = (int) Math.Round( (float) x / (float) (Width / (Frets + 2)), 0 ) - 1;
 				instrumentString = (int) Math.Round( (float) y / (float) (Height / (Strings.Length + 1)), 0 );
 			}
 		}
 
 		protected void DrawFingering( Graphics g, int instrumentString, int fret, Brush brush ) {
-			int spacingFrets = Width / ( Frets + 1 );
+			int spacingFrets = Width / ( Frets + 2 );
 			int spacingStrings = Height / ( Strings.Length + 1 );
 			int stringY = spacingStrings * instrumentString;
-			int fretX = spacingFrets * fret;
+			int fretX = spacingFrets * (fret + 1);
 			
 			g.FillEllipse( brush, fretX - 10, stringY - 10, 20, 20 );
 
 			if( mLeftHanded )
-				fret = mFrets + 1 - fret;
+				fret = mFrets - fret;
 			
 			String label;
 			if( ! mShowFrequencies ) {
 
-				int noteIndex = NoteHelper.Instance().GetOrderedNotes().IndexOf( Strings[instrumentString - 1].Note ) + fret - 1;
+				int noteIndex = NoteHelper.Instance().GetOrderedNotes().IndexOf( Strings[instrumentString - 1].Note ) + fret;
 				int octave = Strings[instrumentString - 1].Octave;
 				
 				while( noteIndex > 11 ) {
@@ -179,8 +168,8 @@ namespace LothianProductions.Notrip {
 					label += "" + octave;
 			} else {
 				// Convert to int to display more cleanly:
-				int step = -36 + ((Strings[instrumentString - 1].Octave - 1) * 12) + NoteHelper.Instance().GetOrderedNotes().IndexOf( Strings[instrumentString - 1].Note );
-				label = (int) (AudioMonitor.ROOT_A4_FREQ * Math.Pow(2, (step + fret - 1) / 12d)) + "";
+				int step = AudioMonitor.TuningToStep( Strings[instrumentString - 1] );
+				label = AudioMonitor.StepToFrequency( step + fret - 1 ) + "";
 			}
 			
 			g.DrawString( label, Font, Brushes.Black, fretX - (g.MeasureString( label, Font ).ToSize().Width / 2), stringY - (g.MeasureString( label, Font ).ToSize().Height / 2) );
@@ -188,11 +177,10 @@ namespace LothianProductions.Notrip {
 
 		private void FingerInstrument( int instrumentString, int fret, bool sustain ) {		
 			if( mLeftHanded )
-				fret = (Frets - fret) + 1;
+				fret = mFrets - fret;
 			
-			if( fret - 1 >= 0 && instrumentString - 1 >= 0 && fret - 1 < Frets && instrumentString - 1 < Strings.Length ) {
-
-				int step = -36 + ((Strings[instrumentString - 1].Octave - 1) * 12) + NoteHelper.Instance().GetOrderedNotes().IndexOf( Strings[instrumentString - 1].Note );
+			if( fret >= 0 && instrumentString - 1 >= 0 && fret <= Frets && instrumentString - 1 < Strings.Length ) {
+				int step = AudioMonitor.TuningToStep( Strings[instrumentString - 1] );
 				double frequency = AudioMonitor.StepToFrequency( step + fret - 1 );
 
 			    if( ! sustain )
@@ -209,7 +197,7 @@ namespace LothianProductions.Notrip {
 			
 			// Add one to the number to find the spacing, so
 			// that there's an equal space at the top and bottom.
-			int spacingFrets = Width / ( Frets + 1 );
+			int spacingFrets = Width / ( Frets + 2 );
 			int spacingStrings = Height / ( Strings.Length + 1 );
 			for( int i = 0; i < Frets; i++ ) {
 				Point[] points = new Point[] {
@@ -244,19 +232,19 @@ namespace LothianProductions.Notrip {
 				stringHanding = new int[]{ Strings.Length - 1, -1, -1 };
 
 			if( ! mLeftHanded )
-				fretHanding = new int[]{ 0, Frets, 1 };
+				fretHanding = new int[]{ 0, Frets + 1, 1 };
 			else
-				fretHanding = new int[]{ Frets - 1, -1, -1 };
+				fretHanding = new int[]{ Frets, -1, -1 };
 
 				
 			for( int i = stringHanding[0]; i != stringHanding[1]; i = i + stringHanding[2] ) {
 				for( int ii = fretHanding[0]; ii != fretHanding[1]; ii = ii + fretHanding[2] ) {		
-					DrawFingering( g, i + 1, ii + 1, Brushes.White );
+					DrawFingering( g, i + 1, ii, Brushes.White );
 
 					if( i == stringHanding[0] ) {
 						int stringY = spacingStrings * (Strings.Length + 1);
 						int fretX = spacingFrets * (ii + 1);
-						String fretNo = Math.Abs( ii + 1 - (mLeftHanded ? mFrets + 1 : 0) ) + "";
+						String fretNo = Math.Abs( ii - (mLeftHanded ? mFrets : 0) ) + "";
 						g.DrawString(
 							fretNo, Font, Brushes.Black,
 							fretX - (g.MeasureString( fretNo, Font ).ToSize().Width / 2),
@@ -289,10 +277,10 @@ namespace LothianProductions.Notrip {
 			// If the fingered note has changed:
 			if( fret != mLastFret || instrumentString != mLastInstrumentString ) {			
 				// Highlight selected note.
-				if( fret - 1 >= 0 && instrumentString - 1 >= 0 && fret - 1 < Frets && instrumentString - 1 < Strings.Length )
+				if( fret >= 0 && instrumentString - 1 >= 0 && fret <= Frets && instrumentString - 1 < Strings.Length )
 					DrawFingering( Graphics.FromHwnd( Handle ), instrumentString, fret, Brushes.SlateBlue );
 				// White out old note.
-				if( mLastFret - 1 >= 0 && mLastInstrumentString - 1 >= 0 && mLastFret - 1 < Frets && mLastInstrumentString - 1 < Strings.Length )
+				if( mLastFret >= 0 && mLastInstrumentString - 1 >= 0 && mLastFret <= Frets && mLastInstrumentString - 1 < Strings.Length )
 				    DrawFingering( Graphics.FromHwnd( Handle ), mLastInstrumentString, mLastFret, Brushes.White );
 				
 				mLastFret = fret;
